c1424cb624b92259e413af890544867af88c50bd
lastovko97@gmail.com <lastovko97@gmail.com>
2020-08-03 23:01:35 +0300

    replace nodemon by node

--- server.js
+++ server.js
@@ -29,10 +29,9 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', (req, res) => { res.send('it IS working') });
+app.get('/', (req, res) => { res.send('it is working') });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, postgres, bcrypt) });
-// app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) });
-app.post('/register', (req, res) => { res.send('trying to register') });
+app.post('/register', (req, res) => { register.handleRegister(req, res, postgres, bcrypt) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, postgres) });
 app.put('/image', (req, res) => { image.handleImage(req, res, postgres) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
